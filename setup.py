b814fd152bedefbea3bee66adc2ef4ac1321bad0
Rossen Georgiev <zoorty@gmail.com>
2015-07-04 20:42:41 +0100

    empty README and setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from codecs import open
+from os import path
+
+from steam import __version__
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='steam',
+    version=__version__,
+    description='Module for interacting with various Steam features',
+    long_description=long_description,
+    url='https://github.com/ValvePython/steam',
+    author='Rossen Georgiev',
+    author_email='hello@rgp.io',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+    ],
+    keywords='valve steam api',
+    packages=['steam'],
+    install_requires=[
+    ],
+    zip_safe=True,
+)
