3ea6b08fc931a7c0730980702687ba25d5803607
Rossen Georgiev <rossen@rgp.io>
2016-02-28 10:50:19 +0000

    up 0.6.3; gevent-eventemitter to v1.2

--- steam/__init__.py
+++ steam/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 __author__ = "Rossen Georgiev"
 
-version_info = (0, 6, 2)
+version_info = (0, 6, 3)
 
 from steam.steamid import SteamID
 from steam.webapi import WebAPI
