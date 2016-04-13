60797b10a2ec304668ac59e1ebf7646408e37c01
Rossen Georgiev <rossen@rgp.io>
2016-04-13 16:30:52 +0100

    bump to v0.6.8

--- steam/__init__.py
+++ steam/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.6.7"
+__version__ = "0.6.8"
 __author__ = "Rossen Georgiev"
 
-version_info = (0, 6, 7)
+version_info = (0, 6, 8)
 
 from steam.steamid import SteamID
 from steam.webapi import WebAPI
