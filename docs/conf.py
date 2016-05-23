ce4be40ae2b802045626748d34f1ebeaaa035b20
Rossen Georgiev <rossen@rgp.io>
2016-05-23 11:26:04 +0100

    docs: remove github ext [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.githubpages',
+#    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
