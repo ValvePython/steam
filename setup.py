2c5177acfc23a779dda3a80825d553dfa5b4bf54
Rossen Georgiev <rossen@rgp.io>
2016-05-27 14:30:02 +0100

    link stable instead of latest docs on pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ import sys
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+    long_description = f.read()\
+                        .replace('io/en/latest/?badge=latest', 'io/en/stable/?badge=stable')\
+                        .replace('projects/steam/badge/?version=latest', 'projects/steam/badge/?version=stable')
 with open(path.join(here, 'steam/__init__.py'), encoding='utf-8') as f:
     __version__ = f.readline().split('"')[1]
 
