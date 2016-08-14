cb3d7ea68ba5aab5bfaa2f55857e2e3071e3887c
Rossen Georgiev <rossen@rgp.io>
2016-08-14 11:23:37 +0100

    bump v0.8.7
    [ci skip]

--- steam/__init__.py
+++ steam/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.8.6"
+__version__ = "0.8.7"
 __author__ = "Rossen Georgiev"
 
-version_info = (0, 8, 6)
+version_info = (0, 8, 7)
 
 from steam.steamid import SteamID
 from steam.globalid import GlobalID
