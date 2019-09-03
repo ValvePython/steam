ValvePython/steam

commit b47f267aa11533b451bb251bef4e6d1d7079d44b

CMClient: move _LOG attr to class level

steam/core/cm.py
@@ -55,6 +55,7 @@ class CMClient(EventEmitter):
     EVENT_EMSG = 0
     """All incoming messages are emitted with their :class:`.EMsg` number.
     """
+    _LOG = logging.getLogger("CMClient")
 
     PROTOCOL_TCP = 0                        #: TCP protocol enum
     PROTOCOL_UDP = 1                        #: UDP protocol enum
@@ -80,7 +81,6 @@ class CMClient(EventEmitter):
     _LOG = None
 
     def __init__(self, protocol=PROTOCOL_TCP):
-        self._LOG = logging.getLogger("CMClient")
         self.cm_servers = CMServerList()
 
         if protocol == CMClient.PROTOCOL_TCP:

commit 09f4f51a287ee7aec1f159c7e8098add5f14bed3

bump to v1.0.0-alpha6
fix bug introduced in b47f267aa11533b451bb251bef4e6d1d7079d44b

steam/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0-alpha5"
+__version__ = "1.0.0-alpha6"
 __author__ = "Rossen Georgiev"
 
-version_info = (1, 0, 0, 'alpha', 5)
+version_info = (1, 0, 0, 'alpha', 6)

steam/core/cm.py
@@ -55,7 +55,6 @@ class CMClient(EventEmitter):
     EVENT_EMSG = 0
     """All incoming messages are emitted with their :class:`.EMsg` number.
     """
-    _LOG = logging.getLogger("CMClient")
 
     PROTOCOL_TCP = 0                        #: TCP protocol enum
     PROTOCOL_UDP = 1                        #: UDP protocol enum
@@ -78,7 +77,7 @@ class CMClient(EventEmitter):
 
     _recv_loop = None
     _heartbeat_loop = None
-    _LOG = None
+    _LOG = logging.getLogger("CMClient")
 
     def __init__(self, protocol=PROTOCOL_TCP):
         self.cm_servers = CMServerList()