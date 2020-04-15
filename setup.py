48e6f43a238b63f3d1f503e41b01e5f24600911d
Rossen Georgiev <rossen@rgp.io>
2020-04-15 22:49:59 +0100

    setup.py: add py3.7 and 3.8 as supported versions

--- setup.py
+++ setup.py
@@ -51,6 +51,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
     keywords='valve steam steamid api webapi steamcommunity',
