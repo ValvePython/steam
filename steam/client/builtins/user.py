0d5609a553949668a5082cba45d750780c2891ed
Rossen Georgiev <rossen@rgp.io>
2020-04-18 22:51:00 +0100

    fix undefined for set_ui_mode()

--- steam/client/builtins/user.py
+++ steam/client/builtins/user.py
@@ -1,6 +1,6 @@
 from weakref import WeakValueDictionary
 from steam.client.user import SteamUser
-from steam.enums import EPersonaState, EChatEntryType, EType
+from steam.enums import EPersonaState, EChatEntryType, EType, EClientUIMode
 from steam.enums.emsg import EMsg
 from steam.core.msg import MsgProto
 from steam.util import proto_fill_from_dict
